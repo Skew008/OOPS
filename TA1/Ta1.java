3263cac4c75df9304411b138645413f3f75061f7
Skew008 <113368301+Skew008@users.noreply.github.com>
2023-05-21 11:39:54 +0530

    Create Ta1.java

---
+++ TA1/Ta1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package shreyansh.oop.ta1;
+
+/**
+ *
+ * @author MSI
+ */
+public class OOPTa1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
