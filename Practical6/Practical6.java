50634bdd819b85c2bb54d94299155b156a5cd36f
Skew008 <113368301+Skew008@users.noreply.github.com>
2023-06-15 00:24:11 +0530

    Update Practical6.java

--- Practical6/Practical6.java
+++ Practical6/Practical6.java
@@ -1,51 +1,80 @@
-package practical6;
+package shreyansh.practical6;
 import java.io.*;
 
 public class Practical6 implements Serializable{
 
     public static void main(String[] args) {
+        File f1 = new File("E:\\netbeans\\Projects\\Practical6\\src\\main\\java\\shreyansh\\practical6\\product.txt");
+        File f2 = new File("E:\\netbeans\\Projects\\Practical6\\src\\main\\java\\shreyansh\\practical6\\customer.txt");
+        int n=0;
         try{
-//        File f1 = new File("C:\\Users\\SRCOEM\\Desktop\\b42\\Practical6\\src\\practical6\\product.txt");
-//        File f2 = new File("C:\\Users\\SRCOEM\\Desktop\\b42\\Practical6\\src\\practical6\\customer.txt");
-        
-        BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
-        FileOutputStream product = new FileOutputStream("C:\\Users\\SRCOEM\\Desktop\\b42\\Practical6\\src\\practical6\\product.txt");
-        FileOutputStream customer = new FileOutputStream("C:\\Users\\SRCOEM\\Desktop\\b42\\Practical6\\src\\practical6\\customer.txt");
-        ObjectOutputStream p1 = new ObjectOutputStream(product);
-        ObjectOutputStream c1 = new ObjectOutputStream(customer);
-        
-        Product p = new Product();
-        Customer c = new Customer(p);
-        String s;
-        
-        System.out.print("Enter Customer name: ");
-        s = console.readLine();
-        c.name=s;
-        System.out.println("Enter Product Details:");
-        System.out.print("Enter Product id: ");
-        s = console.readLine();
-        c.p.id=Integer.parseInt(s);
-        System.out.print("Enter Product Name: ");
-        s = console.readLine();
-        c.p.name=s;
-        System.out.print("Enter Quantity: ");
-        s = console.readLine();
-        c.p.quantity=Integer.parseInt(s);
-        System.out.print("Enter Price: ");
-        s = console.readLine();
-        c.p.price=Double.parseDouble(s);
-        
-        c1.writeObject(c);
-        p1.writeObject(p);
-        
-        c1.flush();
-        p1.flush();
-        c1.close();
-        p1.close();
-        product.close();
-        customer.close();
-        System.out.println(c);
-        System.out.println(p);
+            BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
+            FileOutputStream product = new FileOutputStream(f1);
+            FileOutputStream customer = new FileOutputStream(f2);
+            ObjectOutputStream p1 = new ObjectOutputStream(product);
+            ObjectOutputStream c1 = new ObjectOutputStream(customer);
+            
+            for(int i=0; i<1; i++)
+            {
+                String s;
+                System.out.print("Enter number of products:");
+                s = console.readLine();
+                n = Integer.parseInt(s);
+                Product[] p = new Product[n];
+                Customer c = new Customer(p);
+
+                System.out.print("Enter Customer name: ");
+                s = console.readLine();
+                c.name=s;
+                System.out.println("Enter Product Details:");
+                for(int j=0; j<n; j++)
+                {
+                    p[j] = new Product();
+                    System.out.print("Enter Product id: ");
+                    s = console.readLine();
+                    c.p[j].id=Integer.parseInt(s);
+                    System.out.print("Enter Product Name: ");
+                    s = console.readLine();
+                    c.p[j].name=s;
+                    System.out.print("Enter Quantity: ");
+                    s = console.readLine();
+                    c.p[j].quantity=Integer.parseInt(s);
+                    System.out.print("Enter Price: ");
+                    s = console.readLine();
+                    c.p[j].price=Double.parseDouble(s);
+                    p1.writeObject(p[j]);
+                }
+                c1.writeObject(c);
+            }
+            
+            c1.flush();
+            p1.flush();
+            c1.close();
+            p1.close();
+            product.close();
+            customer.close();
+        }
+        catch(IOException e){
+            System.out.println(e);
+        }
+        try{
+            //FileInputStream product = new FileInputStream(f1);
+            FileInputStream customer = new FileInputStream(f2);
+            //ObjectInputStream p1 = new ObjectInputStream(product);
+            ObjectInputStream c1 = new ObjectInputStream(customer);
+            
+            for(int i = 0; i<1; i++)
+            {
+                Product[] p = new Product[n];
+                for(int j=0; j<n; j++){p[j]=new Product();}
+                Customer c = new Customer(p);
+
+                c=(Customer)c1.readObject();
+                System.out.println(c);
+                for(int j=0; j<n; j++){System.out.println(c.p[j]);}
+            }
+            //product.close();
+            customer.close();
         }
         catch(Exception e){
             System.out.println(e);
