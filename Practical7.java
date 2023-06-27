8f022ae25cf3060b1352328848346aa0de5a23a5
Skew008 <113368301+Skew008@users.noreply.github.com>
2023-06-27 21:02:54 +0530

    Add files via upload

---
+++ Practical7.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package shreyansh.practical7;
+
+/**
+ *
+ * @author MSI
+ */
+public class Practical7 {
+
+    public static void main(String[] args) {
+        MyArray<Integer> i = new MyArray<>(2);
+        i.add(1);
+        i.add(2);
+        i.add(3);
+        i.add(4);
+        i.add(5);
+        i.add(6);
+        i.add(7);
+        i.add(8);
+        i.add(9);
+        i.add(10);
+        //i.swap(1, 0);
+        i.display();
+        
+//        MyArray<String> s = new MyArray<>(2);
+//        s.add("Shreyansh");
+//        s.add("Saurabh");
+//        s.add("Shreyash");
+//        //s.swap(1, 0);
+//        s.display();
+//        
+//        MyArray<Character> c = new MyArray<>(2);
+//        c.add('b');
+//        c.add('a');
+//        c.add('c');
+//        //c.swap(1, 0);
+//        c.display();
+    }
+}
