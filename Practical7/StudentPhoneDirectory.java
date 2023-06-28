041a4d9ebfc3e9fc05702ce4a5d37db80580ccd5
Skew008 <113368301+Skew008@users.noreply.github.com>
2023-06-28 21:16:57 +0530

    Update StudentPhoneDirectory.java

--- Practical7/StudentPhoneDirectory.java
+++ Practical7/StudentPhoneDirectory.java
@@ -1,59 +1,88 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package practical7;
+package shreyansh.practical7;
+import java.util.*;
 
-/**
- *
- * @author MJC
- */
 public class StudentPhoneDirectory {
-    int roll;
-    String name;
-    int sem;
-    String city;
-    String contact;
+    Student[] s;
+    int length = 0;
+    int index = -1;
+    int size;
     
-    StudentPhoneDirectory(int r, String n, int s, String c, String con)
+    StudentPhoneDirectory(int s)
     {
-        roll = r;
-        name = n;
-        sem = s;
-        city = c;
-        contact = con;
+        size = s;
+        this.s = new Student[size];
     }
     
     void addStudent(int r, String n, int s, String c, String con)
     {
-        roll = r;
-        name = n;
-        sem = s;
-        city = c;
-        contact = con;
-    }
-    
-    static void removeStudent(StudentPhoneDirectory s)
-    {
-        s = null;
-    }
-    
-    void viewcity(String c, StudentPhoneDirectory[] s)
-    {
-        int i=0;
-        while(i<s.length)
+        if(index<size)
         {
-            if(c.equals(s[i].city))
+            length++;
+            index++;
+            this.s[index] = new Student(r,n,s,c,con);
+        }
+        ArrayList<Student> al = new ArrayList<>();
+        for(int i=0; i<length; i++){al.add(this.s[i]);}
+        Collections.sort(al, new comp());
+        for(int i=0; i<length; i++){this.s[i] = al.get(i);}
+    }
+    
+    void removeStudent(String n)
+    {
+        int flag=0;
+        for(int i=0; i<s.length; i++)
+        {
+            if(s[i]!=null)
             {
-                System.out.println("Name:"+name+", Phone:"+contact);
+                if(n.equals(s[i].name))
+                {
+                    s[i] = null;
+                    flag++;
+                    length--;
+                } 
             }
         }
+        if(flag==0)
+            System.out.println("No student with name "+n+" exist in direcetory");
+    }
+    
+    void viewcity(String c)
+    {
+        int i=0;
+        int flag=0;
+        while(i<s.length)
+        {
+            if(s[i]!=null){
+            if(c.equals(s[i].city))
+            {
+                System.out.println("Name:"+s[i].name+", Phone:"+s[i].contact);
+                flag++;
+            }}
+            i++;
+        }
+        if(flag==0)
+            System.out.println("No one from "+c+" city");
     }
     
     void viewall()
     {
-            
+        int i=0;
+        int flag=0;
+        while(i<s.length)
+        {
+            if(s[i]!=null)
+            {
+                System.out.println("Name:"+s[i].name+", Phone:"+s[i].contact);
+                flag++;
+            }
+            i++;
+        }
+        if(flag==0)
+            System.out.println("No student");
     }
 }
 
@@ -69,5 +98,17 @@ class myException{
     public String toString()
     {
         return message;
+    }
+}
+
+class comp implements Comparator<Student>{
+    
+    @Override
+    public int compare(Student s1, Student s2)
+    {
+        if(s1.sem!=s2.sem)
+            return s1.sem - s2.sem;
+        else
+            return s1.roll - s2.roll;
     }
 }
